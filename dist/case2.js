25e80c3e38606bb8f18a6d67c0a265fd45642603
China-forrest-w <linhaodewell@163.com>
2021-02-09 11:46:20 +0800

    feat(添加测试用例): then返回Promise

--- dist/case2.js
+++ dist/case2.js
@@ -1,10 +1,18 @@
 /*
-链式回调是promise很重要的特定； 无论成功还是失败then都可以返回结果（1.then出错了走下一个then的错误 2.返回一个普通值也就是不是promise值，那么就会作为下一次then的成功结果）
+链式回调是promise很重要的特定； 无论成功还是失败then都可以返回结果（
+1.then出错了走下一个then的错误 
+2.返回一个普通值也就是不是promise值，那么就会作为下一次then的成功结果
+3.是promise的情况: 采用Promise执行的状态，作为解析的结果
+
+）
 */ 
 let promise = new Promise((resolve, reject) => {
-  reject('ok')
+  // reject('ok')
+  resolve();
 }).then(data => {
-  return 1;
+  return new Promise((resolve, reject) => {
+    resolve(100);
+  })
 }, err => {
 
 })
