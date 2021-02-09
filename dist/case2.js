6cf70121bb40ff358272290963ec42cae2693dda
China-forrest-w <linhaodewell@163.com>
2021-02-09 11:42:13 +0800

    feat(添加测试用例): then返回普通值

--- dist/case2.js
+++ dist/case2.js
@@ -1,15 +1,16 @@
-let promise2 = new Promise((resolve, reject) => {
-  setTimeout(() => {
-    reject();
-  }, 1000)
-}).then((data) => {
+/*
+链式回调是promise很重要的特定； 无论成功还是失败then都可以返回结果（1.then出错了走下一个then的错误 2.返回一个普通值也就是不是promise值，那么就会作为下一次then的成功结果）
+*/ 
+let promise = new Promise((resolve, reject) => {
+  reject('ok')
+}).then(data => {
   return 1;
 }, err => {
-  return 100;
+
 })
 
-promise2.then(res => {
-  console.log(res)
-}, err => {
-  console.log(err);
+promise.then(data => {
+  console.log(data, '**');
+},err => {
+  console.log(err, '----');
 })
